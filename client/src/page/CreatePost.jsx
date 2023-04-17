f7d00a13f37a82c41499df7480a7fd64ce3af93e
gipsy <ter4444@gmail.com>
2023-04-17 14:03:17 +0300

    fixed fetch urls to point to render host

--- client/src/page/CreatePost.jsx
+++ client/src/page/CreatePost.jsx
@@ -48,7 +48,7 @@ const CreatePost = () => {
       setLoading(true);
 
       try {
-        const response = await fetch('http://localhost:8080/api/v1/post', {
+        const response = await fetch('https://dalle-4yj8.onrender.com/api/v1/post', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
